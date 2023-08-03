7dba8c35556036daaa0a4b7d49eabde8549c3865
Benjamín castro <benjamincstr96@gmail.com>
2023-08-02 20:36:46 -0400

    creación proyecto

---
+++ sprint/src/main/java/cl/awakelab/m7/sprint/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.awakelab.m7.sprint;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SprintApplication.class);
+	}
+
+}
---
+++ sprint/src/test/java/cl/awakelab/m7/sprint/SprintApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.awakelab.m7.sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
